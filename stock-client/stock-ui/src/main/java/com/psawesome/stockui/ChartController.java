41d9753ce91ceffc82eea504f82a54144611dd43
PSK <wiv821253@gmail.com>
2020-01-19 19:31:31 +0900

    Symbol Matching

--- stock-client/stock-ui/src/main/java/com/psawesome/stockui/ChartController.java
+++ stock-client/stock-ui/src/main/java/com/psawesome/stockui/ChartController.java
@@ -8,6 +8,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -31,10 +33,15 @@ public class ChartController {
     public void initialize() {
         String symbol = "SYMBOL";
         final PriceSubscriber priceSubscriber1 = new PriceSubscriber(symbol);
+        webClientStockClient.pricesFor(symbol).subscribe(priceSubscriber1);
+
         String symbol2 = "SYMBOL_2";
         final PriceSubscriber priceSubscriber2 = new PriceSubscriber(symbol2);
+        webClientStockClient.pricesFor(symbol2).subscribe(priceSubscriber2);
+
         String symbol3 = "SYMBOL_3";
         final PriceSubscriber priceSubscriber3 = new PriceSubscriber(symbol3);
+        webClientStockClient.pricesFor(symbol3).subscribe(priceSubscriber3);
 
         ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
         data.add(priceSubscriber1.getSeries());
@@ -42,26 +49,21 @@ public class ChartController {
         data.add(priceSubscriber3.getSeries());
         chart.setData(data);
 
-        webClientStockClient.pricesFor(symbol).subscribe(priceSubscriber1);
-        webClientStockClient.pricesFor(symbol2).subscribe(priceSubscriber2);
-        webClientStockClient.pricesFor(symbol3).subscribe(priceSubscriber3);
     }
 
-    private class PriceSubscriber implements Consumer<StockPrice> {
-        private ObservableList<XYChart.Data<String, Double>> seriesData = FXCollections.observableArrayList();
-        private final String symbol;
-        private final XYChart.Series<String, Double> series;
+    private static class PriceSubscriber implements Consumer<StockPrice> {
+        private ObservableList<Data<String, Double>> seriesData = FXCollections.observableArrayList();
+        private final Series<String, Double> series;
 
         public PriceSubscriber(String symbol) {
-            this.symbol = symbol;
-            series = new XYChart.Series<>(seriesData);
+            series = new Series<>(symbol, seriesData);
         }
 
 
         @Override
         public void accept(StockPrice stockPrice) {
             Platform.runLater(() ->
-                    seriesData.add(new XYChart.Data<>(String.valueOf(stockPrice.getTime().getSecond()), stockPrice.getPrice())
+                    seriesData.add(new Data<>(String.valueOf(stockPrice.getTime().getSecond()), stockPrice.getPrice())
                     ));
         }
 
