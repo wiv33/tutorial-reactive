31bbf5a5d21469e7d135d34fe27c24ddc5c9c3fe
PSK <wiv821253@gmail.com>
2020-01-19 19:04:36 +0900

    add series

--- stock-client/stock-client/src/main/java/com/psawesome/stockclient/ClientConfiguration.java
+++ stock-client/stock-client/src/main/java/com/psawesome/stockclient/ClientConfiguration.java
@@ -1,6 +1,6 @@
 package com.psawesome.stockclient;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,7 +19,7 @@ public class ClientConfiguration {
     }
 
     @Bean
-    @ConditionalOnBean
+    @ConditionalOnMissingBean
     public WebClient webClient() {
         return WebClient.builder().build();
     }
--- stock-client/stock-ui/src/main/java/com/psawesome/stockui/ChartController.java
+++ stock-client/stock-ui/src/main/java/com/psawesome/stockui/ChartController.java
@@ -1,9 +1,16 @@
 package com.psawesome.stockui;
 
+import com.psawesome.stockclient.StockPrice;
 import com.psawesome.stockclient.WebClientStockClient;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 /**
  * package: PACKAGE_NAME
@@ -11,12 +18,29 @@ import org.springframework.stereotype.Component;
  * DATE: 2020-01-19 일요일 18:35
  */
 @Component
-public class ChartController {
+public class ChartController implements Consumer<StockPrice> {
     @FXML
     public LineChart<String, Double> chart;
     private WebClientStockClient webClientStockClient;
+    private ObservableList<XYChart.Data<String, Double>> seriesData = FXCollections.observableArrayList();
 
     public ChartController(WebClientStockClient webClientStockClient) {
         this.webClientStockClient = webClientStockClient;
+    }
+
+    @FXML
+    public void initialize() {
+        ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
+        data.add(new XYChart.Series<>(seriesData));
+        chart.setData(data);
+
+        webClientStockClient.pricesFor("SYMBOL").subscribe(this);
+    }
+
+    @Override
+    public void accept(StockPrice stockPrice) {
+        Platform.runLater(() ->
+                seriesData.add(new XYChart.Data<>(String.valueOf(stockPrice.getTime().getSecond()), stockPrice.getPrice())
+        ));
     }
 }
