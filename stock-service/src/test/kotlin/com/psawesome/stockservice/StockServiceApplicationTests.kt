8f491f4372e08ccd8ed92de672678643732bfe69
PSK <wiv821253@gmail.com>
2020-01-19 17:31:38 +0900

    init project

---
+++ stock-service/src/test/kotlin/com/psawesome/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.psawesome.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
